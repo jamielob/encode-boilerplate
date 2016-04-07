b08631cf5f05bd59c2cc8c73631542ca1790ba5d
Jamie Loberman <jamie@beautifulcoding.com>
2016-04-06 18:42:06 -0700

    Show/hide CSS and better classnames

--- packages/tab-view/tab-view.js
+++ packages/tab-view/tab-view.js
@@ -15,12 +15,12 @@ Tracker.autorun(function () {
 	//If we've got the param
 	if (tab) {
 		//Save it
-		Session.set('tabViewCurrent', 'tabView-' + tab);
+		Session.set('tabViewCurrent', 'tab-view-' + tab);
 		//Unhide the tabs
-		Session.set('tabViewHide', 'tabView-show');		
+		Session.set('tabViewVisible', 'tab-view-show');		
 	} else {
 		//Hide the tabs
-		Session.set('tabViewHide', 'tabView-hide');		
+		Session.set('tabViewVisible', 'tab-view-hide');		
 	}
 });
 
