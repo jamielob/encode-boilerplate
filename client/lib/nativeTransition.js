fda3601b31c70ec158fc8c02d2ca2b37cc6e2bf4
Jamie Loberman <jamie@beautifulcoding.com>
2016-04-10 00:08:12 -0700

    Added example default comments

--- client/lib/nativeTransition.js
+++ client/lib/nativeTransition.js
@@ -1,2 +1,8 @@
-nativeTransition.defaults.direction = "down";
-nativeTransition.defaults.type = "flip";
+// nativeTransition.defaults.type = "slide";  
+// nativeTransition.defaults.direction = "left";
+// nativeTransition.defaults.duration = 400;
+// nativeTransition.defaults.slowdownfactor = 3;
+// nativeTransition.defaults.iosdelay = 60;
+// nativeTransition.defaults.androiddelay = 70;
+// nativeTransition.defaults.fixedPixelsTop = 0;
+// nativeTransition.defaults.fixedPixelsBottom = 0;
