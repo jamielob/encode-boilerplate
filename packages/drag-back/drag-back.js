0c985059d11aba4cce11d5a728550f135077492d
Jamie Loberman <jamie@beautifulcoding.com>
2016-04-29 20:21:56 -0700

    Removing drag-back

--- packages/drag-back/drag-back.js
+++
@@ -1,5 +0,0 @@
-// Write your package code here!
-
-// Variables exported by this module can be imported by other packages and
-// applications. See drag-back-tests.js for an example of importing.
-export const name = 'drag-back';
