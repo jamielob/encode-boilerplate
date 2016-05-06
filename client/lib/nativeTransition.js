2287cda986be084c6cca5c6cd95b1962a2e0967d
Jamie Loberman <jamie@beautifulcoding.com>
2016-05-05 23:57:00 -0700

    Adding back button intercept example

--- client/lib/nativeTransition.js
+++ client/lib/nativeTransition.js
@@ -1,8 +1,22 @@
-// nativeTransition.defaults.type = "slide";  
+// nativeTransition.defaults.type = "slide";
 // nativeTransition.defaults.direction = "left";
 nativeTransition.defaults.duration = 300;
 // nativeTransition.defaults.slowdownfactor = 3;
 // nativeTransition.defaults.iosdelay = 60;
  nativeTransition.defaults.androiddelay = 175;
 // nativeTransition.defaults.fixedPixelsTop = 0;
-// nativeTransition.defaults.fixedPixelsBottom = 0;
+// nativeTransition.defaults.fixedPixelsBottom = 0;'
+
+
+//Listen for hardware back button
+document.addEventListener("backbutton", (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+
+  //Initialize the transition
+  nativeTransition.transition({ direction: 'right' });
+
+  //Go back
+  tabHistory.goBack();
+}, false);
+
